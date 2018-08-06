f34dbf01a92424db1f8959bed0bd4188c2fe71c4
angiatiani <41975226+angiatiani@users.noreply.github.com>
2018-08-06 18:21:52 +0700

    Update SAMSUNG.py

--- SAMSUNG.py
+++ SAMSUNG.py
@@ -2020,7 +2020,7 @@ def lineBot(op):
                                         msg_ = settings["mentionPesan"].split("@!")
                                         return sendMention(to, sender, "Auto Reply\n" + msg_[0], msg_[1])
                                     userid = "https://line.me/ti/p/~" + client.profile.userid				
-                                    sendMentionFooter(receiver, sender, "aih" ,"\n{}".format(str(settings['mentionPesan'])), userid, "http://dl.profile.line-cdn.net/"+client.getContact(sender).pictureStatus, client.getContact(sender).displayName))
+                                    sendMentionFooter(receiver, sender, "aih" ,"\n{}".format(str(settings['mentionPesan'])), userid, "http://dl.profile.line-cdn.net/"+client.getContact(sender).pictureStatus, client.getContact(sender).displayName)
                                 break
                 if 'MENTION' in msg.contentMetadata.keys() != None:
                     if settings["notag"] == True:
